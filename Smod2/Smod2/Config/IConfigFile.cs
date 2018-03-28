c67b6ee82ea4f053438f845e41e8e58a2efdddb8
Dankrushen <henryclose@gmail.com>
2018-03-27 20:59:11 -0400

    Added new Config value types

--- Smod2/Smod2/Config/IConfigFile.cs
+++ Smod2/Smod2/Config/IConfigFile.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+
 namespace Smod2
 {
 	public interface IConfigFile
@@ -6,6 +8,13 @@ namespace Smod2
 		int GetIntValue(string key, int def);
 		string GetStringValue(string key, string def);
 		bool GetBoolValue(string key, bool def);
-		string[] GetListValue(string key);
+		string[] GetListValue(string key, string[] def, bool randomValues = false);
+		string[] GetListValue(string key, bool randomValues = false);
+		int[] GetIntListValue(string key, int[] def, bool randomValues = false);
+		int[] GetIntListValue(string key, bool randomValues = false);
+		Dictionary<string, string> GetDictValue(string key, Dictionary<string, string> def, bool randomValues = false, char splitChar = ':');
+		Dictionary<string, string> GetDictValue(string key, bool randomValues = false, char splitChar = ':');
+		Dictionary<int, int> GetIntDictValue(string key, Dictionary<int, int> def, bool randomValues = false, char splitChar = ':');
+		Dictionary<int, int> GetIntDictValue(string key, bool randomValues = false, char splitChar = ':');
 	}
 }
