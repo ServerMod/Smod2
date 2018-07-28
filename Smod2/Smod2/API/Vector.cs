4bb86abb624a90626391dcadffea401a9bab1d49
ShingekiNoRex <327653624@qq.com>
2018-07-28 16:11:07 +0800

    Simplified code

--- Smod2/Smod2/API/Vector.cs
+++ Smod2/Smod2/API/Vector.cs
@@ -16,65 +16,41 @@
 		/// <summary>  
 		///  Shorthand for writing new Vector(0, 0, 0).
 		/// </summary> 
-		public static Vector Zero
-		{
-			get => new Vector(0, 0, 0);
-		}
+		public static Vector Zero => new Vector(0, 0, 0);
 
 		/// <summary>  
 		///  Shorthand for writing new Vector(1, 1, 1).
 		/// </summary> 
-		public static Vector One
-		{
-			get => new Vector(1, 1, 1);
-		}
+		public static Vector One => new Vector(1, 1, 1);
 
 		/// <summary>  
 		///  Shorthand for writing new Vector(0, 0, 1).
 		/// </summary> 
-		public static Vector Forward
-		{
-			get => new Vector(0, 0, 1);
-		}
+		public static Vector Forward => new Vector(0, 0, 1);
 
 		/// <summary>  
 		///  Shorthand for writing new Vector(0, 0, -1).
 		/// </summary> 
-		public static Vector Back
-		{
-			get => new Vector(0, 0, -1);
-		}
+		public static Vector Back => new Vector(0, 0, -1);
 
 		/// <summary>  
 		///  Shorthand for writing new Vector(0, 1, 0).
 		/// </summary> 
-		public static Vector Up
-		{
-			get => new Vector(0, 1, 0);
-		}
+		public static Vector Up => new Vector(0, 1, 0);
 
 		/// <summary>  
 		///  Shorthand for writing new Vector(0, -1, 0).
 		/// </summary> 
-		public static Vector Down
-		{
-			get => new Vector(0, -1, 0);
-		}
+		public static Vector Down => new Vector(0, -1, 0);
 
 		/// <summary>  
 		///  Shorthand for writing new Vector(1, 0, 0).
 		/// </summary> 
-		public static Vector Right
-		{
-			get => new Vector(1, 0, 0);
-		}
+		public static Vector Right => new Vector(1, 0, 0);
 
 		/// <summary>  
 		///  Shorthand for writing new Vector(-1, 0, 0).
 		/// </summary> 
-		public static Vector Left
-		{
-			get => new Vector(-1, 0, 0);
-		}
+		public static Vector Left => new Vector(-1, 0, 0);
 	}
 }
