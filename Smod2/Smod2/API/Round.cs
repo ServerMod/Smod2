9cb35de69bb9f53db36396377273452ddb3701ca
ShingekiNoRex <327653624@qq.com>
2018-09-03 16:52:29 +0800

    Added API
    Round.AddNTFUnit
    Round.MTFRespawn
    Round.RestartRound

--- Smod2/Smod2/API/Round.cs
+++ Smod2/Smod2/API/Round.cs
@@ -17,5 +17,8 @@ namespace Smod2.API
 		public abstract RoundStats Stats { get; }
 		public abstract void EndRound();
 		public abstract int Duration { get; }
+		public abstract void AddNTFUnit(string unit);
+		public abstract void MTFRespawn(bool isCI);
+		public abstract void RestartRound();
 	}
 }
