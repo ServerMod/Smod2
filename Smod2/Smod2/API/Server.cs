5cf31787396961838b7a5abbff3961c14b91d09e
ShingekiNoRex <327653624@qq.com>
2018-09-14 17:34:00 +0800

    Added API Server.PlayerListTitle

--- Smod2/Smod2/API/Server.cs
+++ Smod2/Smod2/API/Server.cs
@@ -20,6 +20,7 @@ namespace Smod2.API
 		public abstract int MaxPlayers { get; set; }
 		public abstract bool Verified { get; }
 		public abstract bool Visible { get; set; }
+		public abstract string PlayerListTitle { get; set; }
 
 		public abstract List<Player> GetPlayers(string filter = "");
 		public abstract List<Connection> GetConnections(string filter = "");
