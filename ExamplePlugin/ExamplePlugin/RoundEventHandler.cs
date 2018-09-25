d9542fe212cb7e2e6a78cb2cbbf07684a45a058f
Evan <Rnen10@gmail.com>
2018-09-25 04:31:02 +0200

    Fixed Whitespaces
    ( Spaces & Tabs combo is bad >:c )

--- ExamplePlugin/ExamplePlugin/RoundEventHandler.cs
+++ ExamplePlugin/ExamplePlugin/RoundEventHandler.cs
@@ -5,8 +5,8 @@ using Smod2.Events;
 
 namespace Smod.TestPlugin
 {
-    class RoundEventHandler : IEventHandlerRoundStart, IEventHandlerRoundEnd
-    {
+	class RoundEventHandler : IEventHandlerRoundStart, IEventHandlerRoundEnd
+	{
 		private Plugin plugin;
 
 		public RoundEventHandler(Plugin plugin)
@@ -20,15 +20,15 @@ namespace Smod.TestPlugin
 		}
 
 		public void OnRoundStart(RoundStartEvent ev)
-        {
-            plugin.Info("ROUND START EVENT CALLER");
-            plugin.Info("Players: " + ev.Server.GetPlayers().Count);
-            foreach (Player player in ev.Server.GetPlayers())
-            {
-                // Print the player info and then their class info
-                plugin.Info(player.ToString());
-                plugin.Info(player.TeamRole.ToString());
-            }
-        }
-    }
+		{
+			plugin.Info("ROUND START EVENT CALLER");
+			plugin.Info("Players: " + ev.Server.GetPlayers().Count);
+			foreach (Player player in ev.Server.GetPlayers())
+			{
+			// Print the player info and then their class info
+			plugin.Info(player.ToString());
+			plugin.Info(player.TeamRole.ToString());
+			}
+		}
+	}
 }
