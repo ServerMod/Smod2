0ea557ca12bc31acfb42584dfd29098da2525e08
laserman367 <36745405+laserman367@users.noreply.github.com>
2018-10-31 09:15:08 +0100

    Update Map.cs
    added AnnounceCustomMessage

--- Smod2/Smod2/API/Map.cs
+++ Smod2/Smod2/API/Map.cs
@@ -26,6 +26,7 @@ namespace Smod2.API
 		public abstract List<TeslaGate> GetTeslaGates();
 		public abstract void AnnounceNtfEntrance(int scpsLeft, int mtfNumber, char mtfLetter);
 		public abstract void AnnounceScpKill(string scpNumber, Player killer = null);
+		public abstract void AnnounceCustomMessage(string words);
 		public abstract void SetIntercomSpeaker(Player player);
 		public abstract Player GetIntercomSpeaker();
 	}
