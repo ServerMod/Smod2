6e22c930ddf8965cffe5de3b89b263f64bedb669
Dankrushen <henryclose@gmail.com>
2019-02-03 03:38:37 -0500

    Update UserGroup.cs
    Add "UserGroup#Cover"
    Add "UserGroup#HiddenByDefault"

--- Smod2/Smod2/API/UserGroup.cs
+++ Smod2/Smod2/API/UserGroup.cs
@@ -2,9 +2,11 @@
 {
 	public abstract class UserGroup
 	{
-		public abstract string Text { get; }
 		public abstract string Color { get; }
+		public abstract string Text { get; }
 		public abstract ulong Permissions { get; }
+		public abstract bool Cover { get; }
+		public abstract bool HiddenByDefault { get; }
 		public abstract string Name { get; }
 	}
 }
