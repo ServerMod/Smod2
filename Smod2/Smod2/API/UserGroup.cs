4589c7ca0997fdefe60af9ee8acbf8a12278badc
Dankrushen <henryclose@gmail.com>
2019-02-03 03:28:07 -0500

    UserGroup changes
    - Renamed "UserGroup#Name" to "UserGroup#Text" (the text of the tag to display)
    - Added "UserGroup#Name" (name of the group, not tag to display)

--- Smod2/Smod2/API/UserGroup.cs
+++ Smod2/Smod2/API/UserGroup.cs
@@ -2,8 +2,9 @@
 {
 	public abstract class UserGroup
 	{
-		public abstract string Name { get; }
+		public abstract string Text { get; }
 		public abstract string Color { get; }
 		public abstract ulong Permissions { get; }
+		public abstract string Name { get; }
 	}
 }
