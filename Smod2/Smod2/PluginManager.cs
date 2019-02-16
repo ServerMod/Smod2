2eb444f96e8af41b4e1ff99eab471ddddee61df5
Luke Janik <luke04janik@gmail.com>
2019-02-16 15:27:54 -0600

    bump version since master did

--- Smod2/Smod2/PluginManager.cs
+++ Smod2/Smod2/PluginManager.cs
@@ -15,8 +15,8 @@ namespace Smod2
 	public class PluginManager
 	{
 		public static readonly int SMOD_MAJOR = 3;
-		public static readonly int SMOD_MINOR = 2;
-		public static readonly int SMOD_REVISION = 4;
+		public static readonly int SMOD_MINOR = 3;
+		public static readonly int SMOD_REVISION = 1;
 		public static readonly string SMOD_BUILD = "A";
 
 		public static readonly string DEPENDENCY_FOLDER = "dependencies";
