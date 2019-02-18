ab07707799fce2eb308f684c42a7fa4594097b75
Luke Janik <luke04janik@gmail.com>
2019-02-17 18:49:59 -0600

    add ConfigSetting constructors that have SettingsType auto-filled

--- Smod2/Smod2/Config/ConfigSetting.cs
+++ Smod2/Smod2/Config/ConfigSetting.cs
@@ -1,4 +1,6 @@
-﻿namespace Smod2.Config
+﻿using System.Collections.Generic;
+
+namespace Smod2.Config
 {
 	public enum SettingType
 	{
@@ -77,15 +79,24 @@
 			this.primaryUser = primaryUser;
 			this.description = description;
 		}
+		
+		public ConfigSetting(string key, bool defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.BOOL, primaryUser, description) { }
+		public ConfigSetting(string key, string defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.STRING, primaryUser, description) { }
+		public ConfigSetting(string key, string[] defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.LIST, primaryUser, description) { }
+		public ConfigSetting(string key, int defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.NUMERIC, primaryUser, description) { }
+		public ConfigSetting(string key, int[] defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.NUMERIC_LIST, primaryUser, description) { }
+		public ConfigSetting(string key, float defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.FLOAT, primaryUser, description) { }
+		public ConfigSetting(string key, Dictionary<string, string> defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.DICTIONARY, primaryUser, description) { }
+		public ConfigSetting(string key, Dictionary<int, int> defaultValue, bool randomized, bool primaryUser, string description) : this(key, defaultValue, randomized, SettingType.NUMERIC_DICTIONARY, primaryUser, description) { }
 
-		public ConfigSetting(string key, object defaultValue, SettingType type, bool primaryUser, string description)
-		{
-			this.key = key.ToUpper();
-			this.defaultValue = defaultValue;
-			this.randomized = false;
-			this.configType = type;
-			this.primaryUser = primaryUser;
-			this.description = description;
-		}
+		public ConfigSetting(string key, object defaultValue, SettingType type, bool primaryUser, string description) : this(key, defaultValue, false, type, primaryUser, description) { }
+		public ConfigSetting(string key, bool defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.BOOL, primaryUser, description) { }
+		public ConfigSetting(string key, string defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.STRING, primaryUser, description) { }
+		public ConfigSetting(string key, string[] defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.LIST, primaryUser, description) { }
+		public ConfigSetting(string key, int defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.NUMERIC, primaryUser, description) { }
+		public ConfigSetting(string key, int[] defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.NUMERIC_LIST, primaryUser, description) { }
+		public ConfigSetting(string key, float defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.FLOAT, primaryUser, description) { }
+		public ConfigSetting(string key, Dictionary<string, string> defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.DICTIONARY, primaryUser, description) { }
+		public ConfigSetting(string key, Dictionary<int, int> defaultValue, bool primaryUser, string description) : this(key, defaultValue, SettingType.NUMERIC_DICTIONARY, primaryUser, description) { }
 	}
 }
