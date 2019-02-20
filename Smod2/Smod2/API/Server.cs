a7485b0d07e8cf55065c9e193a7097158428cbeb
Luke Janik <luke04janik@gmail.com>
2019-02-20 16:48:50 -0600

    fix GetAppFolder

--- Smod2/Smod2/API/Server.cs
+++ Smod2/Smod2/API/Server.cs
@@ -24,13 +24,13 @@ namespace Smod2.API
 		[Obsolete("Nonfunctional", true)]
 		public abstract bool Visible { get; set; } //Not used whatsoever. Only kept to prevent plugins to have to be recompiled
 		public abstract string PlayerListTitle { get; set; }
-		public abstract string AppFolder { get; }
 
 		public abstract List<Player> GetPlayers(string filter = "");
 		public abstract List<Player> GetPlayers(Role role);
 		public abstract List<Player> GetPlayers(Role[] roles);
 		public abstract List<Connection> GetConnections(string filter = "");
 		public abstract List<TeamRole> GetRoles(string filter = "");
+		public abstract string GetAppFolder(bool shared = false, bool addSeparator = false, bool addPort = false, bool addConfigs = false);
 
 		public abstract bool BanSteamId(string username, string steamId, int duration, string reason = "", string issuer = "Server");
 		public abstract bool BanIpAddress(string username, string ipAddress, int duration, string reason = "", string issuer = "Server");
