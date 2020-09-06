6d16218085f262dfa29218005483edef4f82ac07
laserman367 <36745405+laserman367@users.noreply.github.com>
2020-09-06 13:40:45 +0200

    Remove Name setter and add DisplayedNickname

--- Smod2/API/Player.cs
+++ Smod2/API/Player.cs
@@ -104,7 +104,8 @@ namespace Smod2.API
 		protected bool ShouldCallSetRoleEvent { get => CallSetRoleEvent; } // used in the game
 
 		public abstract TeamRole TeamRole { get; set; }
-		public abstract string Name { get; set; }
+		public abstract string Name { get; }
+		public abstract string DispleyedNickname { get; set; } // Differs from the Name in that it can be modified by server.
 		public abstract string IpAddress { get; }
 		public abstract int PlayerId { get; }
 		public abstract string UserId { get; }
