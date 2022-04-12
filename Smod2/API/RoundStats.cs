72b8cc4b91445b8aeacb4fc8ed383acbe067757d
Karl Essinger <xkaess22@gmail.com>
2022-04-12 23:40:14 +0200

    Added settable parameters to round stats

--- Smod2/API/RoundStats.cs
+++ Smod2/API/RoundStats.cs
@@ -4,22 +4,24 @@ namespace Smod2.API
 {
 	public abstract class RoundStats
 	{
-		public abstract int NTFAlive { get; }
+		public abstract int Kills { get; set; }
+		public abstract int MTFAlive { get; }
+		public abstract int CIAlive { get; }
 		public abstract int ScientistsAlive { get; }
 		public abstract int ScientistsEscaped { get; set; }
 		public abstract int ScientistsDead { get; }
 		public abstract int ScientistsStart { get; }
-		public abstract int ClassDEscaped { get; set; }
-		public abstract int ClassDDead { get; }
-		public abstract int ClassDAlive { get; }
-		public abstract int ClassDStart { get; }
+		public abstract int DClassEscaped { get; set; }
+		public abstract int DClassDead { get; }
+		public abstract int DClassAlive { get; }
+		public abstract int DClassStart { get; }
 		public abstract int Zombies { get; }
+		public abstract int ChangedIntoZombies { get; set; }
 		public abstract int SCPDead { get; }
-		public abstract int SCPKills { get; }
+		public abstract int SCPKills { get; set; }
 		public abstract int SCPAlive { get; }
 		public abstract int SCPStart { get; }
-		public abstract int GrenadeKills { get; }
+		public abstract int GrenadeKills { get; set; }
 		public abstract bool WarheadDetonated { get; }
-		public abstract int CiAlive { get; }
 	}
 }
